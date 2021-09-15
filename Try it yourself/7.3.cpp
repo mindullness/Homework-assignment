2a83942ad316939e1d8e0523bb46560740d6cbfa
maaguaete <65282760+Maaguaete@users.noreply.github.com>
2021-09-15 22:23:30 +0700

    Corrected homework

--- Try it yourself/7.3.cpp
+++ Try it yourself/7.3.cpp
@@ -7,24 +7,26 @@ int main(){
 	printf("*** This program is about to Calculate the difference between the two values ***\n");
 	
 	int a,b, diff; // diff: difference
-	do{	
+	do{
+		
 		printf("\nEnter value a: ");
 		scanf("%d", &a);
 		printf("Enter value b: ");
 		scanf("%d", &b);
 		
-		if(a !=0 && b!=0)
+		if(a==0 && b==0)
 		{
-			diff = abs(a-b);
+			printf("Entering a zero value twice is not wise! Redo it!");
+		} else{
+        	diff = abs(a-b);
 			if(diff == a)
 				printf("Difference is equal to value a: %d.", a);
 			else if(diff == b)
 				printf("Difference is equal to value b: %d.", b);
 			else
 				printf("Difference is not equal to any of the values entered.");
-		} else		
-			printf("Entering a zero value twice is not wise! Redo it!");	
-	
+		}
+
 	} while(a==0 && b==0);
 	
 }
--- Try it yourself/8.3.cpp
+++ Try it yourself/8.3.cpp
@@ -17,17 +17,20 @@ int main(){
 		printf("All the values are equal!");
 	else{
 		
-		if(a>b)
-			if(a>c)
-   				max = a;
-			else
-				max = c;
-		else
-			if(b>c)
-				max = b;
-			else
-				max = c;
-				
+//		if(a>b)
+//			if(a>c)
+//   				max = a;
+//			else
+//				max = c;
+//		else
+//			if(b>c)
+//				max = b;
+//			else
+//				max = c;
+
+		max = (a>b)?a:b;    //max is the highest btw a & b.
+		max = (max>c)?max:c;
+
 		printf("The highest value is %d.", max);
 	}
 	

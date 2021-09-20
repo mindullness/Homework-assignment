34e0d78d86ac0f2c7acfe516d8be7d91f5ea40d3
maaguaete <65282760+Maaguaete@users.noreply.github.com>
2021-09-20 19:06:53 +0700

    Update Consonants in Ss12_2

--- Try it yourself/Session 11 12/12_2.cpp
+++ Try it yourself/Session 11 12/12_2.cpp
@@ -5,31 +5,46 @@ int main(){
 
 	printf("** Count the number of vowels and the number of consonants in a word \n\n**");
 
-	int count=0;
+	int countV=0, countC=0;
 	char word[20];
 	
 	printf("Enter a word: ");
 	scanf("%[^ \n]s", word);
 	
-	for(int i=0; i<strlen(word);i++){
+	int n = strlen(word);
+	// Style using strchr()
+	char vowels[] = "ueoaiUEOAI";
+	for(int i=0; i<n ; i++){
+		if((word[i]>='A'&& word[i]<='Z') || (word[i]>='a'&& word[i]<='z')){
 
-		switch(word[i]){
-			
-			case 'A':
-			case 'a':
-			case 'E':
-			case 'e':
-			case 'I':
-			case 'i':
-			case 'O':
-			case 'o':
-			case 'U':
-			case 'u':
-				count++;
-				break;
-			default:
-				break;
+			if(strchr(vowels, word[i]))
+				countV++;
+			else
+				countC++;
 		}
 	}
-	printf("\nThe number of vowels in the word \"%s\": %d.",word, count);
+	
+
+	printf("\nThe number of vowels in the word \"%s\": %d.",word, countV);
+	printf("\nThe number of consonants in the word \"%s\": %d.",word, countC);
+	
+// Style switch:
+//	for(int i=0; i<n ;i++){
+//		switch(word[i]){
+//			case 'A':
+//			case 'a':
+//			case 'E':
+//			case 'e':
+//			case 'I':
+//			case 'i':
+//			case 'O':
+//			case 'o':
+//			case 'U':
+//			case 'u':
+//				count++;
+//				break;
+//			default:
+//				break;
+//		}
+//	}
 }

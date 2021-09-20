f4cfae9fd4b886e86ef5d7759d7d74e611739e8b
maaguaete <65282760+Maaguaete@users.noreply.github.com>
2021-09-20 19:10:19 +0700

    Update Fibonacii series

--- Try it yourself/Session 9/9.3.cpp
+++ Try it yourself/Session 9/9.3.cpp
@@ -5,9 +5,10 @@ int main(){
 	printf("*** This program is to display the Fibonacci series ***\n\n");
 	
 	int N, sum2pre=0, num1=1, num2=1;
-	printf("Enter the length of the Fibonacci series: ");
+	do{
+	printf("Enter the length of the Fibonacci series(>=3): ");
 	scanf("%d", &N);
-	
+	}while(N<3);
 	printf("\nThe Fibonacci series: ");
 	
 	for(int i=0; i<N-1; i++){

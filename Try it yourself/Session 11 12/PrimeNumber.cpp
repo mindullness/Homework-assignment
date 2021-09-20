761514c37e80d13590e7778631c14db9883d8fae
maaguaete <65282760+Maaguaete@users.noreply.github.com>
2021-09-20 19:45:52 +0700

    Update

--- Try it yourself/Session 11 12/PrimeNumber.cpp
+++ Try it yourself/Session 11 12/PrimeNumber.cpp
@@ -1,15 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <cmath>
 int main(){
 
 	printf("** Check a number whether it is prime or not **\n\n");
 	
-	int a;
+	int a, flag=1;
 	printf("Input a number: ");
 	scanf("%d", &a);
 	
-	if(a%2 != 0)
+	for(int i=2; i<=sqrt(a); i++){
+		if(a%i==0){
+			flag=0;
+			printf("%d can divide for: %d.\n", a, i);
+			break;
+		}
+	}
+	
+	if(flag)
 		printf("Number %d is a prime number!", a);
 	else
 		printf("Number %d is NOT a prime number!", a);
--- Try it yourself/Session 9/9.3.cpp
+++ Try it yourself/Session 9/9.3.cpp
@@ -6,9 +6,10 @@ int main(){
 	
 	int N, sum2pre=0, num1=1, num2=1;
 	do{
-	printf("Enter the length of the Fibonacci series(>=3): ");
+	printf("Enter the length of the Fibonacci series (length > 2): ");
 	scanf("%d", &N);
 	}while(N<3);
+	
 	printf("\nThe Fibonacci series: ");
 	
 	for(int i=0; i<N-1; i++){
@@ -19,5 +20,29 @@ int main(){
 		num2 = sum2pre;
 	}
 	printf("%d.", num1);
+	
+	//Cach 2:
+	printf("\n*** This program is to display the Fibonacci series (ver 2) ***\n\n");
+	int n;
+	do{
+	printf("Enter the length of the Fibonacci series (length > 2): ");
+	scanf("%d", &n);
+	}while(N<3);
+	
+	int fibo[n];
+	fibo[0]= fibo[1] = 1;
+	
+	for(int i=2; i<n; i++){
+
+		fibo[i] = fibo[i-1] + fibo[i-2];
+	}
+	
+	printf("\nThe Fibonacci series: ");
+	
+	for(int i=0; i<n-1; i++){
+
+		printf("%d, ", fibo[i]);
+	}
+	printf("%d.", fibo[n-1]);
 }
 

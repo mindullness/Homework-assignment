149cddd850abcb0d416b309da0d25ccb9abd0c76
maaguaete <tigermaaguaete@gmail.com>
2021-10-30 18:27:54 +0700

    Update Pretest3

--- Pretest/3/function_display.js
+++ Pretest/3/function_display.js
@@ -1,15 +1,17 @@
-function display() {
-    var fname = document.getElementById("fname").value;
-    var gender = document.getElementById("gender").value;
-    var address = document.getElementById("address").value;
-    var email = document.getElementById("email").value;
-    var phone = document.getElementById("telNo").value;
-    var qualif = document.getElementById("qualif").value;
+function storeValue() {
+    sessionStorage.setItem("i_name", document.getElementById("name").value);
+    sessionStorage.setItem("i_gender", document.getElementById("gender").value);
+    sessionStorage.setItem("i_address", document.getElementById("address").value);
+    sessionStorage.setItem("i_email", document.getElementById("email").value);
+    sessionStorage.setItem("i_telNo", document.getElementById("telNo").value);
+    sessionStorage.setItem("i_qualif", document.getElementById("qualif").value);
+}
 
-    document.getElementById("dname").innerHTML = fname;
-    document.getElementById("dgender").innerHTML = gender;
-    document.getElementById("daddress").innerHTML = address;
-    document.getElementById("demail").innerHTML = email;
-    document.getElementById("dtelNo").innerHTML = phone;
-    document.getElementById("dqualif").innerHTML = qualif;
+function display() {
+    document.getElementById("d_name").innerHTML = sessionStorage.getItem("i_name");
+    document.getElementById("d_gender").innerHTML = sessionStorage.getItem("i_gender");
+    document.getElementById("d_address").innerHTML = sessionStorage.getItem("i_address");
+    document.getElementById("d_email").innerHTML = `<a href="#">${sessionStorage.getItem("i_email")}</a>`;
+    document.getElementById("d_telNo").innerHTML = sessionStorage.getItem("i_telNo");
+    document.getElementById("d_qualif").innerHTML = sessionStorage.getItem("i_qualif");
 }
